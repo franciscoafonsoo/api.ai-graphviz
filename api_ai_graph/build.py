74e83d8c89885a8d12d5a9529b7b72eb8e6115c8
sherby <fafonso@greenbyweb.eu>
2017-07-27 16:20:24 +0100

    still buggy

--- api_ai_graph/build.py
+++ api_ai_graph/build.py
@@ -24,18 +24,16 @@ def build_graph(usercase, lintents):
         # if the intent has an event, we create a 'rect' shaped node that demostrastes a connection to the server.
         # in later stages, when an intent connects to this intent, we connected to the 'event' intent instead.
 
+        f.node(x.name, label=x.name + '\n' + "in: " + str(x.contextin) + ' \n ' + "out: " + str(x.contextout))
+
         if x.events:
             f.node(x.events[0], label=x.events[0], shape='rect')
-            f.node(x.name, label=x.name + '\n' + "in: " + str(x.contextin) + ' \n ' + "out: " + str(x.contextout))
             f.edge(x.events[0], x.name)
-        if x.first:
-            f.node(x.name, label=x.name + '\n' + "in: " + str(x.contextin) + ' \n ' + "out: " + str(x.contextout))
+        elif x.first:
             f.edge('true', x.name, label=x.usersays[0])
         elif not x.contextin:
             f.node(x.name, label=x.name + ' \n ' + "out: " + str(x.contextout))
             f.edge('true', x.name, label=x.usersays[0])
-        else:
-            f.node(x.name, label=x.name + '\n' + "in: " + str(x.contextin) + ' \n ' + "out: " + str(x.contextout))
 
     # compare every intent with every other intent
 
@@ -43,16 +41,16 @@ def build_graph(usercase, lintents):
         for source in lintents:
 
             # if the first is equal to the second (overwrite equal function at the intent.py)
+
             if target == source:
+                print('comparar: ' + source.name, target.name)
                 # draw an edge if the user interacts with the api.ai
                 if target.fallback:
                     f.edge(source.name, target.name, label='#')
-                elif target.events:
+                elif target.events and source.webhook:
                     f.edge(source.name, target.events[0], label='event')
-                else:
+                elif target.usersays:
                     f.edge(source.name, target.name, label=target.usersays[0])
-
-                    # draw an edge if the intent is a intentfallback
 
     # noinspection PyArgumentList
     # f.render()
